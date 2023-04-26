ac6a49ab6d2f01b34a24aeb031fa973bd14ee1e0
cwy <1013953614@qq.com>
2023-04-26 18:03:18 +0800

    scss全局注册全局变量

---
+++ src/components/RefreshButton/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <el-button color="#40485b" @click="refRoad">
+    <i class="icon iconfont icon-shuaxin"></i>
+  </el-button>
+</template>
+
+<script lang="ts" setup name="RefreshButton">
+import { inject } from 'vue'
+const reload: any = inject('reload') //注入刷新事件,这里括号中的参数要对应上前面provide中的第一个参数
+
+function refRoad() {
+  reload()
+}
+</script>
+
+<style lang="scss" scoped></style>
